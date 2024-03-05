3750651fa9998828d5dfedc3ef0c48ede1c7b0b5
poornachandran872 <chandru08july2000@gmail.com>
2024-03-05 14:43:57 +0530

    folders added

---
+++ src/components/About.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import AboutImg from '../assets/about.png'
+
+const About = () => {
+  const config  = {
+    line1: 'Hi, My name is Poornachandran. I am a Full stack web developer. I built beautiful Websites with React.js , Tailwind CSS',
+    line2: 'I am proficient in Frontend skills like React.js, Redux, Redux Tool Kit, Axios, Tailwind CSS, SaSS, Css3 and many more.',
+    line3: 'In backend I know Node.js, Express.js, MongoDB, and Mongoose'
+}
+  return (
+    <section id='about' className='flex flex-col md:flex-row bg-secondary px-5'>
+        <div className='md:w-1/2 py-10'>
+            <img src={AboutImg} />
+        </div>
+        <div className='md:w-1/2 flex justify-center'>
+            <div className='flex flex-col justify-center text-white'>
+
+            <h1 className=' text-4xl border-b-4 border-primary w-[170px] m-5 font-bold'>About Me</h1>
+            <p className='pb-5'>{config.line1} </p>
+            <p className='pb-5'>{config.line2}</p>
+            <p className='pb-5'>{config.line3}</p>
+            </div>
+        </div>
+      
+    </section>
+  )
+}
+
+export default About
---
+++ src/components/Contact.jsx
@@ -0,0 +1,18 @@
+
+export default function Contact () {
+
+    const config = {
+        email : 'chandru08july2000@gmail.com',
+        phone: '+91 6374079642'
+    }
+
+    return <section id='contact' className='bg-primary px-5 py-32 text-white'>
+        <div className='flex flex-col items-center'>
+           
+            <h1 className='text-4xl border-b-4  border-secondary mb-5 w-[140px] font-bold'>Contact</h1>
+            <p className='pb-5'>If you want to discuss more in detail, please contact me</p>
+            <p className='py-2'><span className='font-bold'>Email :</span> {config.email}</p>
+            <p className='py-2'><span className='font-bold'>Phone :</span> {config.phone}</p>
+        </div>
+    </section>
+}
