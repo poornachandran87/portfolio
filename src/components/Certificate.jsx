d9f1a1d9291f51083d0feb6ef19b30731d377c05
poornachandran872 <chandru08july2000@gmail.com>
2025-06-30 23:07:18 +0530

    certificates section added

--- src/components/Certificate.jsx
+++ src/components/Certificate.jsx
@@ -6,7 +6,7 @@ export default function Certificate() {
         link2: 'https://drive.google.com/file/d/1dASVKGWnSu-PBBO1SutTF_1Lnz_Y0r77/view?usp=sharing'
     }
 
-    return <section id='/certificate' className='flex flex-col md:flex-row justify-center  px-5 py-5 bg-primary' >
+    return <section id='certificate' className='flex flex-col md:flex-row justify-center  px-5 py-5 bg-primary' >
         <div className='md:w-1/2 flex justify-center'>
             <div className='flex flex-col justify-center text-white'>
                 <h1 className='text-4xl border-b-4 border-secondary mb-5 w-[188px] font-bold'>Certificates</h1>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,6 +11,7 @@ const Header = () => {
         <ul className='flex text-white'>
           <li><a href="#/">Home</a></li>
           <li><a href="#about">About</a></li>
+          <li><a href="#certificate">Certificates</a></li>
           <li><a href="#projects">Projects</a></li>
           <li><a href="#resume">Resume</a></li>
           <li><a href="#contact">Contact</a></li>
@@ -21,6 +22,7 @@ const Header = () => {
 
         <ul onClick={() => setToggleMenu(!toggleMenu)} className='flex flex-col text-white mobile-nav'>
           <li><a href="#/">Home</a></li>
+          <li><a href="#certificate">Certificates</a></li>
           <li><a href="#about">About</a></li>
           <li><a href="#projects">Projects</a></li>
           <li><a href="#resume">Resume</a></li>
